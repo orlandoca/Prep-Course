94288a0da9f7954645fa5605ec6150f521ae4f26
orlandoca <orlandoca647@gmail.com>
2022-04-15 22:16:26 -0400

    creando booleana

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'orlando';
 const nuevoNum = 10;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
